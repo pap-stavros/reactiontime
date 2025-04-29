c5954f4d5d3c86fa4d1b4f3b8dff7f8e2d1793d0
Stavros P. <stayrospapaxris@gmail.com>
2025-04-29 06:07:04 +0300

    Added early flag, knows when you jump the start now. Prevents spam too.

--- t.cpp
+++ t.cpp
@@ -14,7 +14,7 @@ enum GameState { //self explanatory.
 int main() {
     const int w = 888;
     const int h = 666;
-    InitWindow(w, h, "Reaction Time");
+    InitWindow(w, h, "Reaction Time - Stavros P.");
     srand(static_cast<unsigned int>(time(0))); //random seed, for timer.
     
     GameState gs = wait;
@@ -23,6 +23,7 @@ int main() {
     double rt = 0.0;
     double rDelay = 0.0;
     bool clicked = false;
+    bool early = false;
     
     while (!WindowShouldClose()) {
         switch (gs) {
@@ -32,10 +33,16 @@ int main() {
                     rDelay = static_cast<double>(rand() % 3 + 2);
                     sTime = GetTime();
                     clicked = false;
+                    early = false;
                 }
                 break;
                 
-            case red: //gets elapsed time since countdown, changes to green when random delay is reached, resets timer for measurement.
+            case red: //gets elapsed time since countdown, fails early if clicked, or changes to green when random delay is reached.
+                if (IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    early = true;
+                    gs = fail;
+                    break;
+                }
                 cTime = GetTime() - sTime;
                 if (cTime >= rDelay) {
                     gs = green;
@@ -50,27 +57,45 @@ int main() {
                         rt = cTime;
                         gs = results;
                     } else if (cTime > 2.0) {
+                        early = false;
                         gs = fail;
                     }
                 }
-                char liveT[20];
-                sprintf(liveT, "%.3f", cTime);
-                DrawText(liveT,
-                         w/2 - MeasureText(liveT, 33)/2,
-                         h/2 + 30,
-                         33, WHITE);
+                {
+                    char liveT[20];
+                    sprintf(liveT, "%.3f", cTime);
+                    DrawText(liveT,
+                             w/2 - MeasureText(liveT, 33)/2,
+                             h/2 + 30,
+                             33, WHITE);
+                }
                 break;
 
-                
-            case fail:  // tells grandpa hes getting slow
+            case fail:  // tells grandpa hes getting slow or jumped early cause hes seeing things.
                 if (IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                    gs = wait;
+                    if (early) {
+                        gs = red;
+                        rDelay = static_cast<double>(rand() % 3 + 2);
+                        sTime = GetTime();
+                        clicked = false;
+                        early = false;
+                        break;
+                    } else {
+                        gs = wait;
+                    }
                 }
                 ClearBackground(BLACK);
-                DrawText("You can't be that slow...",
-                         w/2 - MeasureText("You can't be that slow...", 30)/2,
-                         h/2 - 30,
-                         30, WHITE);
+                if (early) {
+                    DrawText("You jumped the start...",
+                             w/2 - MeasureText("You jumped the start...", 30)/2,
+                             h/2 - 30,
+                             30, WHITE);
+                } else {
+                    DrawText("You can't be that slow...",
+                             w/2 - MeasureText("You can't be that slow...", 30)/2,
+                             h/2 - 30,
+                             30, WHITE);
+                }
                 DrawText("Press Space or Click to try again",
                          w/2 - MeasureText("Press Space or Click to try again", 20)/2,
                          h/2 + 30,
@@ -97,19 +122,20 @@ int main() {
                 
             case red:
                 ClearBackground(RED);
-                int dotSt;
-                dotSt = ((int)(GetTime() * 2)) % 4;
-                const char* txt;
-                switch(dotSt) {
-                    case 0: txt = "WAIT"; break;
-                    case 1: txt = "WAIT."; break;
-                    case 2: txt = "WAIT.."; break;
-                    case 3: txt = "WAIT..."; break; // thought it'd be cool
+                {
+                    int dotSt = ((int)(GetTime() * 2)) % 4;
+                    const char* txt;
+                    switch(dotSt) {
+                        case 0: txt = "WAIT"; break;
+                        case 1: txt = "WAIT."; break;
+                        case 2: txt = "WAIT.."; break;
+                        default: txt = "WAIT..."; break; // thought it'd be cool
+                    }
+                    DrawText(txt,
+                             w/2 - MeasureText("WAIT...", 60)/2,
+                             h/2 - 30,
+                             60, WHITE);
                 }
-                DrawText(txt,
-                         w/2 - MeasureText("WAIT...", 60)/2, // if "WAIT..." is replaced with txt, it recenters each time.
-                         h/2 - 30,
-                         60, WHITE);
                 break;
                 
             case green:
@@ -122,12 +148,14 @@ int main() {
                 
             case results:
                 ClearBackground(BLACK);
-                char rText[50];
-                sprintf(rText, "Your reaction time: %.3f sec", rt);
-                DrawText(rText,
-                         w/2 - MeasureText(rText, 30)/2,
-                         h/2 - 15,
-                         30, RAYWHITE);
+                {
+                    char rText[50];
+                    sprintf(rText, "Your reaction time: %.3f sec", rt);
+                    DrawText(rText,
+                             w/2 - MeasureText(rText, 30)/2,
+                             h/2 - 15,
+                             30, RAYWHITE);
+                }
                 DrawText("Press Space or Click to try again",
                          w/2 - MeasureText("Press Space or Click to try again", 20)/2,
                          h/2 + 30,
